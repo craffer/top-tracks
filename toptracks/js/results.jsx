1216d6083729cdf1c06c22d9131c3ffc6cb966c9
Conor Rafferty <craffer@umich.edu>
2020-05-12 23:09:01 -0400

    Set tracks layout

--- toptracks/js/results.jsx
+++ toptracks/js/results.jsx
@@ -9,19 +9,20 @@ export default class Results extends React.Component {
     let resultsDivs = [];
     this.props.results.forEach((result, index) => {
       resultsDivs.push(
-        <li className="list-group-item d-flex align-items-center">
+        <li
+          key={result.name}
+          className="list-group-item d-flex align-items-center"
+        >
           <img className="album-art mr-3" src={result.album.images[1].url} />
+          <h1 className="mr-3">{index + 1}.</h1>
           <div>
-            <h5 className="mb-0">
-              {index + 1}. {result.name}
-            </h5>
-            <small className="text-muted">
+            <h4>{result.name}</h4>
+            <p className="text-muted mb-0">
               {result.artists.map((res) => {
-                console.log(res);
-                return <div key={res.name}>{res.name}</div>;
-              })}
-            </small>
-            <p className="mb-1 text-muted">{result.album.name}</p>
+                return res.name;
+              })}{" "}
+              – {result.album.name}
+            </p>
           </div>
           <div className="ml-auto">
             <h2>{result.popularity}</h2>
