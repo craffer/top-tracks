4f47610d315f9f93b6527247b932ba6d55e5604d
Conor Rafferty <craffer@umich.edu>
2020-05-19 12:14:55 -0400

    Add track subcomponent

--- toptracks/js/tracks.jsx
+++ toptracks/js/tracks.jsx
@@ -8,63 +8,76 @@ export default class Tracks extends React.Component {
   render() {
     let resultsDivs = [];
     this.props.tracks.forEach((track, index) => {
-      resultsDivs.push(
-        <li
-          key={track.spotify_id}
-          className="list-group-item d-flex align-items-center w-100"
-        >
-          <a href={track.external_url} target="_blank">
-            <img
-              className="album-art mr-2 mr-md-3 shadow-sm"
-              src={track.album.images[1].url}
-            />
-          </a>
-          <h1 className="mr-2 mr-md-3 ranking">{index + 1}.</h1>
-          <div className="col-7 px-0">
-            <h4 className="song-name mb-0 mb-md-2 scroll-text text-nowrap w-100">
-              <a
-                className="text-dark"
-                href={track.external_url}
-                target="_blank"
-              >
-                {track.name}
-              </a>
-            </h4>
-            <div className="text-muted scroll-text text-nowrap w-100">
-              {track.artists.map((res, index, arr) => {
-                // for the last element, we don't want a comma and space after
-                let nameDisplay = res.name;
-                if (arr.length - 1 !== index) {
-                  nameDisplay += ", ";
-                }
-                return (
-                  <a
-                    className="text-muted album-artist"
-                    target="_blank"
-                    key={res.spotify_id}
-                    href={res.external_url}
-                  >
-                    {nameDisplay}
-                  </a>
-                );
-              })}{" "}
-              –{" "}
-              <a
-                className="text-muted album-artist"
-                href={track.album.external_url}
-                target="_blank"
-              >
-                {track.album.name}
-              </a>
-            </div>
-          </div>
-          <div className="ml-auto d-flex flex-column align-items-center">
-            <small>Score:</small>
-            <h2 className="score">{track.popularity}</h2>
-          </div>
-        </li>
-      );
+      resultsDivs.push(<Track track={track} ranking={index + 1} />);
     });
     return <ul className="list-group mb-4">{resultsDivs}</ul>;
+  }
+}
+
+class Track extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let track = this.props.track;
+    return (
+      <li
+        key={track.spotify_id}
+        className="list-group-item d-flex align-items-center w-100"
+      >
+        <a href={track.external_url} target="_blank">
+          <img
+            className="album-art mr-2 mr-md-3 shadow-sm"
+            src={track.album.images[1].url}
+          />
+        </a>
+        <h1 className="mr-2 mr-md-3 ranking">{this.props.ranking}.</h1>
+        <div className="col-7 px-0">
+          <h4
+            className="song-name mb-0 mb-md-2 scroll-text text-nowrap w-100"
+            ref={(el) => (this.songName = el)}
+          >
+            <a className="text-dark" href={track.external_url} target="_blank">
+              {track.name}
+            </a>
+          </h4>
+          <div
+            className="text-muted scroll-text text-nowrap w-100"
+            ref={(el) => (this.artistAndAlbum = el)}
+          >
+            {track.artists.map((res, index, arr) => {
+              // for the last element, we don't want a comma and space after
+              let nameDisplay = res.name;
+              if (arr.length - 1 !== index) {
+                nameDisplay += ", ";
+              }
+              return (
+                <a
+                  className="text-muted album-artist"
+                  target="_blank"
+                  key={res.spotify_id}
+                  href={res.external_url}
+                >
+                  {nameDisplay}
+                </a>
+              );
+            })}{" "}
+            –{" "}
+            <a
+              className="text-muted album-artist"
+              href={track.album.external_url}
+              target="_blank"
+            >
+              {track.album.name}
+            </a>
+          </div>
+        </div>
+        <div className="ml-auto d-flex flex-column align-items-center">
+          <small>Score:</small>
+          <h2 className="score">{track.popularity}</h2>
+        </div>
+      </li>
+    );
   }
 }
